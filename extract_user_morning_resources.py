0f1d7f3e1bc91de855b250bec65494a99d29f274
William Loock <145708222+WXscho@users.noreply.github.com>
2025-04-01 04:27:08 -0500

    Added output html file

---
+++ extract_user_morning_resources.py
@@ -0,0 +1,23 @@
+# Fn double shift: use when Quickly find any file, action, class,
+# symbol, tool window, or setting in PyCharm, in your project,
+# and in the current Git repository.
+
+#TODO what data is inputted
+
+## object  user_brief_resource ##
+# title of resource
+# description of resource
+# URl
+class UserBriefResource:
+  # constructor
+
+
+
+
+#TODO what is done with the data
+
+# display on a html page
+
+#TODO what is the output
+
+
