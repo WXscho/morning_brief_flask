e89bdc9f7fc72aab475d86f16bb8dba492b61b1f
William Loock <145708222+WXscho@users.noreply.github.com>
2025-04-06 02:48:12 -0500

    Trying flask stuff

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-
+import os
+print(os.listdir('templates'))
 app = Flask(__name__)
 
-@app.route('/brief')
+@app.route('/')
 def brief():
-  return render_template('output_user_brief.html')
+  return render_template('base.html')
 
 
 if __name__ == '__main__':
